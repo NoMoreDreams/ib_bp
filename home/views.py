6080f17331aef4820f44877d879ef8c343983942
Pavol <pavol_almasi@centrum.sk>
2023-04-25 00:40:29 +0200

    login app

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, "home/home_page.html", {})
+
+
+@login_required(login_url="/admin")
+def authorized(request):
+    return render(request, "home/authorized.html", {})
