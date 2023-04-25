c47cd136aae6fd9a0a82844aabefb8659e9b3239
Pavol <pavol_almasi@centrum.sk>
2023-04-25 16:44:47 +0200

    add banking app

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import UserLoginView
 
 urlpatterns = [
-    path('login/', views.login, name="login_page"),
+    # path('login/', views.login, name="login_page"),
+    path('login/', UserLoginView.as_view(), name="login_page"),
     path('login/verification/', views.email_verification, name="verification"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
-def login(request):
-    return render(request, "login/login_page.html", {})
+# def login(request):
+#     return render(request, "login/login_page.html", {})
+
+class UserLoginView(LoginView):
+    template_name = "login/login_page.html"
 
 
 def email_verification(request):
